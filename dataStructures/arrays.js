f0da2aab3293ec1b4f3110e48959bdbf40fde623
Mark-scope <agyemangmark1@gmail.com>
2024-09-16 23:33:57 +0000

    datastructures

---
+++ dataStructures/arrays.js
@@ -0,0 +1,14 @@
+let students = ['Ali', 'Omar', 90, true];
+console.log(students);
+
+students.pop(); // removes the last element
+console.log(students);
+
+students.push('Ahmed');
+console.log(students);  // adds the last element
+
+students.unshift('Ahmed'); // adds the first element
+console.log(students);
+
+students.shift(); // removes the first element
+console.log(students)
